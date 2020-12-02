b62b5918225b65651e915f00bbd1ad89fa155bbd
Julien Linzas <linzas.julien@gmail.com>
2020-12-02 16:04:19 +0100

    update devise & room controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,21 @@
 class RoomsController < ApplicationController
   def new
-
+    @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to room_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:player_number, :price_filter)
+  end
+
 end
