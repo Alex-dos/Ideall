eda8b28200fe2cae4c1a0939e84c08349215ec9d
Jean-Loup Peres <doc.jprs@gmail.com>
2020-12-02 16:06:59 +0100

    seed fini

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_120834) do
-
+ActiveRecord::Schema.define(version: 2020_12_02_135154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +39,8 @@ ActiveRecord::Schema.define(version: 2020_12_02_120834) do
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "player_number"
+    t.integer "player_number_min"
+    t.integer "player_number_max"
     t.index ["category_id"], name: "index_games_on_category_id"
   end
 
