7eef238e86886934edb654780b04cd7f4d8c701e
Julien Linzas <linzas.julien@gmail.com>
2020-12-04 12:35:11 +0100

    descriptif room

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_225650) do
-
+ActiveRecord::Schema.define(version: 2020_12_03_120525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
