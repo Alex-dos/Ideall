8813bfea5e5a773e253a41b613647c708442b780
Paul-Henri Horvilleur <paulo238@wanadoo.fr>
2020-12-04 10:51:00 +0100

    test sur requêtes API et db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_144811) do
+ActiveRecord::Schema.define(version: 2020_12_03_120525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_12_02_144811) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "player_number_min"
     t.integer "player_number_max"
+    t.integer "appid"
     t.index ["category_id"], name: "index_games_on_category_id"
   end
 
