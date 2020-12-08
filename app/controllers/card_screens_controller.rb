905bf56abd529ea1283447678a2c62f05b8c02cd
Paul-Henri Horvilleur <paulo238@wanadoo.fr>
2020-12-08 19:06:13 +0100

    Barres de votes avec Julien et Maxime

--- app/controllers/card_screens_controller.rb
+++ app/controllers/card_screens_controller.rb
@@ -3,7 +3,11 @@ class CardScreensController < ApplicationController
     @room = Room.find(params[:room_id])
     @choice_categories = ChoiceCategory.where(room_id: params[:room_id])
     @choice_categories_with_votes = @choice_categories
-    @choice_categories = @choice_categories.select {|choice_category| choice_category unless choice_category.update_by[0] == current_user.name || choice_category.update_by[1] == current_user.name || choice_category.update_by[2] == current_user.name || choice_category.update_by[3] == current_user.name || choice_category.update_by[4] == current_user.name || choice_category.update_by[5] == current_user.name || choice_category.update_by[6] == current_user.name || choice_category.update_by[7] == current_user.name}[0..2]
+    @choice_categories = @choice_categories.select do |choice_category|
+                           unless choice_category.update_by[0] == current_user.name || choice_category.update_by[1] == current_user.name || choice_category.update_by[2] == current_user.name || choice_category.update_by[3] == current_user.name || choice_category.update_by[4] == current_user.name || choice_category.update_by[5] == current_user.name || choice_category.update_by[6] == current_user.name || choice_category.update_by[7] == current_user.name
+                             choice_category
+                           end
+                         end [0..2]
 
     if all_voted(@choice_categories_with_votes)
       list_games(@choice_categories_with_votes).each do |game|
@@ -12,7 +16,6 @@ class CardScreensController < ApplicationController
     end
 
     redirect_to room_ecran2_path(@room) if all_voted(@choice_categories_with_votes)
-
   end
 
   def ecran2
@@ -30,19 +33,16 @@ class CardScreensController < ApplicationController
   def all_voted(choice_array)
     @all_voted = []
     choice_array.each do |choice_category|
-      if choice_category.update_by.length ==  choice_category.room.player_number
-        @all_voted << true
-      end
+      @all_voted << true if choice_category.update_by.length == choice_category.room.player_number
     end
     @all_voted.length == choice_array.length
   end
 
   def list_games(choice_array)
-    @result_category = choice_array.sort_by{|choice_category| choice_category.rank }[7..8]
+    @result_category = choice_array.sort_by { |choice_category| choice_category.rank }[7..8]
     @result1 = @result_category.map do |result|
-      result.category.games.where(price: nil).map {|game| game}
+      result.category.games.where(price: nil).map { |game| game }
     end
     return @result1.flatten
   end
-
 end
--- app/controllers/choice_categories_controller.rb
+++ app/controllers/choice_categories_controller.rb
@@ -6,32 +6,30 @@ class ChoiceCategoriesController < ApplicationController
     @choice_category.rank += @rank.to_i
     @choice_category.update_by << current_user.name
     @choice_category.save
-    data = {category: @choice_category.category.id, rank: @choice_category.rank}
+    data = { category: @choice_category.category.id, rank: @choice_category.rank, player_number: @room.player_number }
+
     RoomChannel.broadcast_to(
       @room, data.to_json
-      )
+    )
 
-    if all_voted(@room.choice_categories) 
+    if all_voted(@room.choice_categories)
       RoomChannel.broadcast_to(
-        @room, { 
-        head: 302, # redirection code, just to make it clear what you're doing
-        path: room_ecran2_path(@room) # you'll need to use url_helpers, so include them in your file
-      }.to_json
+        @room, {
+          head: 302, # redirection code, just to make it clear what you're doing
+          path: room_ecran2_path(@room) # you'll need to use url_helpers, so include them in your file
+        }.to_json
       )
     end
 
     redirect_to room_ecran1_path(@room)
-    
   end
 
   private
-  
+
   def all_voted(choice_array)
     @all_voted = []
     choice_array.each do |choice_category|
-      if choice_category.update_by.length ==  choice_category.room.player_number
-        @all_voted << true
-      end
+      @all_voted << true if choice_category.update_by.length == choice_category.room.player_number
     end
     @all_voted.length == choice_array.length
   end
