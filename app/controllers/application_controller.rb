1627dd123a89399fd7dee3ec71803116e70e18a0
Paul-Henri Horvilleur <paulo238@wanadoo.fr>
2020-12-09 11:42:13 +0100

    Première étape de la checklist Title tag is set and exceeds 20 chars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  def default_url_options
+    { host: ENV["www.ideall.cf"] || "localhost:3000" }
+  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
