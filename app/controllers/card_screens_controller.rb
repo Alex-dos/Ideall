e95193ab0fe70f7ffb8b5621f341464e9c044411
Julien Linzas <linzas.julien@gmail.com>
2020-12-10 12:25:24 +0100

    fix ampoules et votes

--- app/controllers/card_screens_controller.rb
+++ app/controllers/card_screens_controller.rb
@@ -4,7 +4,7 @@ class CardScreensController < ApplicationController
     @choice_categories = ChoiceCategory.where(room_id: params[:room_id])
     @choice_categories_with_votes = @choice_categories
     @choice_categories = @choice_categories.select {|choice_category| choice_category unless choice_category.update_by[0] == current_user.name || choice_category.update_by[1] == current_user.name || choice_category.update_by[2] == current_user.name || choice_category.update_by[3] == current_user.name || choice_category.update_by[4] == current_user.name || choice_category.update_by[5] == current_user.name || choice_category.update_by[6] == current_user.name || choice_category.update_by[7] == current_user.name}[0..2]
-
+    # @choice_categories_select = @choice_categories_with_votes.sort_by { |choice_category| choice_category.rank }[0..4]
 
     
     @voted = {}
@@ -74,12 +74,20 @@ class CardScreensController < ApplicationController
   end
 
   def list_games(choice_array)
-    @result_category = choice_array.sort_by { |choice_category| choice_category.rank }[7..8]
+    @result_category = choice_array.sort_by { |choice_category| choice_category.rank }.last(2)
     @result1 = @result_category.map do |result|
       if @room.price_filter == "Gratuit"
-        result.category.games.where(price: nil).map { |game| game }
+        if @room.player_number == 1
+          result.category.games.where(price: nil, solo:"Solo").map { |game| game }
+        else
+          result.category.games.where(price: nil, multi:"Multijoueur").map { |game| game }
+        end
       else
-        result.category.games.map { |game| game }
+        if @room.player_number == 1
+        result.category.games.where(solo:"Solo").map { |game| game }
+        else
+          result.category.games.where(multi:"Multijoueur").map { |game| game }
+        end
       end
     end
     return @result1.flatten
